1a44feb4857a1da197dfc4b64834de31f02b155c
sen <gallas.post@gmail.com>
2014-04-06 15:38:54 +0400

    hw_stack_&_queue_commit

---
+++ day_09/stack.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+
+
+class Stack(object):
+
+    def push(self, x):
+        if self.stack[1] == self.stack[0]:
+            print "stack overflow"
+            return self.stack
+        self.stack[1] += 1
+        self.stack += [x]
+        return self.stack
+
+    def pop(self):
+        if self.stack[1] == 0:
+            print "stack empty"
+            return self.stack
+        self.stack[1] -= 1
+        x = self.stack[-1]
+        self.stack = self.stack[:-1]
+        return self.stack, x
+
+    def top(self):
+        if self.stack[1] == self.stack[0]:
+            return self.stack[self.stack[1]+1], "stack overflow"
+        elif self.stack[1] == 0:
+            return None, "stack empty"
+        else:
+            return self.stack[self.stack[1]+1], ""
+
+    def __str__(self):
+        return "Stack: {0} \t top: {1} \t {2}".format(self.stack, self.top()[0], self.top()[1])
+
+    def __init__(self, size):
+        """В первом элементе списка хранится размер стека, во втором - кол-во элементов в стеке"""
+        self.stack = [size, 0]
+
+if __name__ == '__main__':
+    s1 = Stack(5)
+    n = 6
+    print s1
+    print "-" * 30
+    for i in range(n):
+        s1.push(i)
+        print s1
+        print "-" * 30
+    for i in range(n):
+        s1.pop()
+        print s1
+        print "-" * 30
